e04534d058dad0fd894ef9eef9547542afa8dfd8
Thiago Fontes <thiago93br@gmail.com>
2017-07-26 23:43:25 -0300

    Music is now working

--- bot.js
+++ bot.js
@@ -4,23 +4,21 @@
 
 // Import the discord.js module
 const Discord = require('discord.js');
-const music = require('discord.js-music');
+const music = require('discord.js-music-v11');
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
+const token = 'Your token here!';
+
 //Songs config
 music(client, {
-	prefix: '-',        // Prefix of '-'.
-	global: false,      // Server-specific queues.
-	maxQueueSize: 10,   // Maximum queue size of 10.
+	prefix: '-',       // Prefix of '-'.
+	global: false,     // Server-specific queues.
+	maxQueueSize: 10,  // Maximum queue size of 10.
 	clearInvoker: true, // If permissions applicable, allow the bot to delete the messages that invoke it (start with prefix)
-    	channel: 'music'    // Name of voice channel to join. If omitted, will instead join user's voice channel.
+    channel: 'music'   // Name of voice channel to join. If omitted, will instead join user's voice channel.
 });
-
-
-// The token of your bot - https://discordapp.com/developers/applications/me
-const token = '';
 
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
@@ -39,6 +37,11 @@ client.on('message', message => {
 
   if (message.content === 'invite'){
     message.channel.send('invite link here');
+  }
+  
+  if (message.content === 'love'){
+      message.channel.send('Sending love back...');
+      message.channel.send('Love sent');
   }
 });
 
